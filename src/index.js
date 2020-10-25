a891865be0572aaf109812f34d86bd59e891343b
arturocas98 <arturocastro98@hotmail.com>
2020-10-24 20:15:34 -0500

    añadido tailwind al proyecto y añadido react router, creaciones del componente ordenes

--- src/index.js
+++ src/index.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import './css/main.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+// import { Router } from 'react-router';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <App />
+    </Router>
+      
+  
+      
+    
   </React.StrictMode>,
   document.getElementById('root')
 );
